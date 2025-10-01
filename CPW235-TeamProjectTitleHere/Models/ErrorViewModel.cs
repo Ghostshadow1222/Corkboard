ff8b5283911888fb1d5400ff5072305bd03316dd
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-09-30 18:02:35 -0700

    Add project files.

---
+++ CPW235-TeamProjectTitleHere/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPW235_TeamProjectTitleHere.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
