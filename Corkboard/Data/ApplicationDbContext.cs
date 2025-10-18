166bec7a6491c6b777b405c7ff032457c424089d
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-10-17 20:26:15 -0700

    Customize Identity with UserAccount model
    Updated `ApplicationDbContext` to use `IdentityDbContext<UserAccount>` for custom user model support. Modified `Program.cs` to configure `UserAccount` as the default identity model and updated the connection string in `appsettings.json` to use the `CorkboardDb` database. These changes enable a custom identity setup and database configuration for the application.

--- Corkboard/Data/ApplicationDbContext.cs
+++ Corkboard/Data/ApplicationDbContext.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Corkboard.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Corkboard.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<UserAccount>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
     }
