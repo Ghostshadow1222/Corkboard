9f2dad053dd82fbbd8157a569eb60eac7665c4de
Aiden Richard <115966042+aiden-richard@users.noreply.github.com>
2025-11-06 16:27:18 -0800

    Issue #27 create chat controller (#30)
    * Created Chat controller that can accept a chat id
    
    * Added index view for chat controller. For now it just displays the id from the url as a proof of concept

---
+++ Corkboard/Controllers/ChatController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Corkboard.Controllers;
+public class ChatController : Controller
+{
+	[HttpGet("chat/{id?}")]
+	public IActionResult Index(string? id = null)
+	{
+		// Expose the optional id to the view for later use.
+		ViewData["ChatId"] = id;
+		return View();
+	}
+}
