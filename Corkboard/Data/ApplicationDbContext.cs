fa12b87839fc2d6c7c536f40d5ca96812b40e63d
Aiden <richardaiden2006@gmail.com>
2025-11-06 17:45:17 -0800

    use file scoped namespace in db context file

--- Corkboard/Data/ApplicationDbContext.cs
+++ Corkboard/Data/ApplicationDbContext.cs
@@ -2,17 +2,16 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Corkboard.Data
-{
-    public class ApplicationDbContext : IdentityDbContext<UserAccount>
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+namespace Corkboard.Data;
 
-		public DbSet<Server> Servers { get; set; }
-		public DbSet<ServerMember> ServerMembers { get; set; }
-		public DbSet<Channel> Channels { get; set; }
-		public DbSet<Message> Messages { get; set; }
-	}
+public class ApplicationDbContext : IdentityDbContext<UserAccount>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+	public DbSet<Server> Servers { get; set; }
+	public DbSet<ServerMember> ServerMembers { get; set; }
+	public DbSet<Channel> Channels { get; set; }
+	public DbSet<Message> Messages { get; set; }
 }
