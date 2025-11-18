c472fd4605bd2903b6824fa9a0fe1caee3425ac1
Aiden <richardaiden2006@gmail.com>
2025-11-18 11:53:32 -0800

    Add ServerInviteViewModel

---
+++ Corkboard/Models/ServerInviteViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Corkboard.Models;
+
+public class ServerInviteViewModel
+{
+	/// <summary>
+	/// Foreign key to the <see cref="Server"/> this invite is for.
+	/// </summary>
+	[Required]
+	public int ServerId { get; set; }
+
+	/// <summary>
+	/// Foreign key to the <see cref="UserAccount"/> who is invited to the server (optional).
+	/// </summary>
+	public string? UserId { get; set; }
+
+	public int DaysUntilExpires { get; set; } = 7;
+
+	public bool OneTimeUse { get; set; } = true;
+}
