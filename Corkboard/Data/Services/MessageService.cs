5e4ffd1a4a36ae783738191d854e0f83c0d44332
Aiden Richard <richardaiden2006@gmail.com>
2025-12-07 21:06:18 -0800

    Implement GetMessagesBeforeTimestampAsync and update GetMessagesForChannelAsync to return Message objects

--- Corkboard/Data/Services/MessageService.cs
+++ Corkboard/Data/Services/MessageService.cs
@@ -20,8 +20,18 @@ public interface IMessageService
 	/// Retrieves DTOs for messages in the specified channel ordered chronologically.
 	/// </summary>
 	/// <param name="channelId">Channel id.</param>
+	/// <param name="limit">Maximum number of messages to retrieve.</param>
 	/// <returns>List of message DTOs.</returns>
-	Task<List<MessageDto>> GetMessagesForChannelAsync(int channelId, int? limit = null);
+	Task<List<Message>> GetMessagesForChannelAsync(int channelId, int? limit = null);
+
+	/// <summary>
+	/// Retrieves DTOs for messages in the specified channel before a given timestamp.
+	/// </summary>
+	/// <param name="channelId">Channel id.</param>
+	/// <param name="beforeTimestamp">Retrieve messages created before this timestamp.</param>
+	/// <param name="limit">Maximum number of messages to retrieve.</param>
+	/// <returns>List of message DTOs ordered chronologically.</returns>
+	Task<List<Message>> GetMessagesBeforeTimestampAsync(int channelId, DateTime beforeTimestamp, int limit = 50);
 }
 
 /// <summary>
@@ -41,21 +51,33 @@ public class MessageService : IMessageService
 	}
 
 	/// <inheritdoc/>
-	public async Task<List<MessageDto>> GetMessagesForChannelAsync(int channelId, int? limit = null)
+	public async Task<List<Message>> GetMessagesForChannelAsync(int channelId, int? limit = null)
 	{
-		List<MessageDto> messages = await _context.Messages
+		List<Message> messages = await _context.Messages
 			.Where(m => m.ChannelId == channelId)
 			.Include(m => m.Sender)
-			.OrderBy(m => m.CreatedAt)
+			.OrderByDescending(m => m.CreatedAt)
 			.Take(limit ?? int.MaxValue)
-			.Select(m => new MessageDto
-			{
-				Text = m.MessageContent,
-				SenderUsername = m.Sender.UserName!,
-				Timestamp = m.CreatedAt
-			})
 			.ToListAsync();
 
+		// Reverse to get chronological order (oldest to newest)
+		messages.Reverse();
+		return messages;
+	}
+
+	/// <inheritdoc/>
+	public async Task<List<Message>> GetMessagesBeforeTimestampAsync(int channelId, DateTime beforeTimestamp, int limit = 50)
+	{
+		// Fetch messages older than the given timestamp, ordered descending, then reverse
+		List<Message> messages = await _context.Messages
+			.Where(m => m.ChannelId == channelId && m.CreatedAt < beforeTimestamp)
+			.Include(m => m.Sender)
+			.OrderByDescending(m => m.CreatedAt)
+			.Take(limit)
+			.ToListAsync();
+
+		// Reverse to get chronological order (oldest to newest)
+		messages.Reverse();
 		return messages;
 	}
 
